ac9be01df62ab0f759725ec6e711d569f928219f
Farshad Sahafzadeh <f.sahafzadeh@gmail.com>
2019-12-04 14:04:13 +0330

    Uncomment members

--- publish.js
+++ publish.js
@@ -352,10 +352,10 @@ function buildNav(members) {
   // TODO: as needed, comment back in later
   // nav += buildMemberNav(members.externals, 'Externals', seen, linktoExternal);
   // nav += buildMemberNav(members.events, 'Events', seen, linkto);
-  // nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
+  nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
   // nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
   // nav += buildMemberNav(members.tutorials, 'Tutorials', seenTutorials, linktoTutorial);
-  // nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
+  nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
 
   if (members.globals.length) {
     members.globals.forEach(function (g) {
